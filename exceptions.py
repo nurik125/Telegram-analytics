eba53f668bc91242bda4054f5a59bd06fa6c7c76
bs8303918-lgtm <bs8303918@gmail.com>
2026-04-01 19:41:23 +0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,11 @@
 class ModelNotFoundError(Exception):
-  def __init__(self, *args: object) -> None:
-    super().__init__(*args)
-    
+    """Raised when a Groq model ID is invalid or not available on the account."""
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+ 
+ 
 class ModelNotSpecifiedError(Exception):
-  def __init__(self, *args: object) -> None:
-    super().__init__(*args)
+    """Raised when no model ID is provided to a service that requires one."""
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+ 
